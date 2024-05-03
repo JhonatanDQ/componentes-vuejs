f6e5be0d07ca4a3c488b400148741d07429861a6
Jhonatan Quiroga <jdquirogape@ufpso.edu.co>
2024-05-03 17:01:38 -0500

    Componente astro con vue

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+let counter = ref(0)
+
+function add(){
+    counter.value++
+    console.log('Hola xd' , counter)
+}
+</script>
+
+<template>
+   <div @click="add" class="button bg-blue-200 border-black py-1 rounded-full mt-2 cursor-pointer px-2 max-w-40 mx-3 text-center select-none">
+   Numero de clicks: {{ counter }}
+   </div>
+</template>
